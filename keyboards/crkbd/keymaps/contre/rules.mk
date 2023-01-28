cd0ddb3290e3a4b6f212b423f1375de38414e61a
contre95 <lucascontre95@gmail.com>
2023-01-29 00:59:11 +0100

    Disable led

--- keyboards/crkbd/keymaps/contre/rules.mk
+++ keyboards/crkbd/keymaps/contre/rules.mk
@@ -1,5 +1,5 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
+MOUSEKEY_ENABLE = yes    # Mouse key
+RGBLIGHT_ENABLE = no   # Enable WS2812 RGB underlight.
+OLED_ENABLE     = no
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
