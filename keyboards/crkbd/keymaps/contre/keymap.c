8ed2d00cdf57d5041b2afed263762919365de30a
contre95 <lucascontre95@gmail.com>
2023-06-09 12:06:43 +0200

    QMK

--- keyboards/crkbd/keymaps/contre/keymap.c
+++ keyboards/crkbd/keymaps/contre/keymap.c
@@ -27,15 +27,15 @@ enum crkbd_layers {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT_split_3x6_3(             
+  [_QWERTY] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_ESC,    KC_Q,    KC_W,   KC_E,  KC_R,  KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC, 
+      KC_ESC,    KC_Q,    KC_W,   KC_E,  KC_R,  KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       MT(MOD_LSFT, KC_TAB), KC_A, KC_S, KC_D, KC_F,  KC_G,                       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,   KC_Z,    KC_X,   KC_C,  KC_V,  KC_B,                            KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  MT(MOD_RSFT, KC_ENT),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                KC_LALT,   MO(_LOWER),  KC_LGUI,         KC_SPC,  MO(_RAISE) ,KC_RALT                      
+                                KC_LALT,   MO(_LOWER),  KC_LGUI,         KC_SPC,  MO(_RAISE) ,KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -65,11 +65,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ADJUST] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      QK_BOOT, KC_F10, KC_F11, KC_F7, KC_F8, KC_F9,                             KC_7, KC_8, KC_9  , KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, KC_BSPC,
+      QK_BOOT, KC_F10, KC_F11, KC_F7, KC_F8, KC_F9,                             KC_7, KC_8, KC_9, KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_CAPS_LOCK, KC_F12, XXXXXXX, KC_F4, KC_F5, KC_F6,                       KC_4, KC_5, KC_6, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC_BRIGHTNESS_UP,
+      KC_CAPS_LOCK, KC_F12, XXXXXXX, KC_F4, KC_F5, KC_F6,                       KC_4, KC_5, KC_6, KC_COMM, XXXXXXX, KC_BRIGHTNESS_UP,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, KC_F1, KC_F2, KC_F3,                           KC_1 , KC_2, KC_3  , KC_COMM, KC_DOT,  KC_BRIGHTNESS_DOWN,
+      XXXXXXX, XXXXXXX, XXXXXXX, KC_F1, KC_F2, KC_F3,                           KC_1 , KC_2, KC_3, KC_DOT, KC_SLSH, KC_BRIGHTNESS_DOWN,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LALT, _______,  KC_SPC,     KC_MEDIA_PLAY_PAUSE, _______, KC_0
                                       //`--------------------------'  `--------------------------'
